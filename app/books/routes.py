2baa11d0085e6c0b776364467956d2cb6da4391d
Nathan Schrader <natecschrader@gmail.com>
2024-07-21 13:10:00 -0700

    add HTTPException for unsuccessful endpoint call

--- app/books/routes.py
+++ app/books/routes.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Query, HTTPException
 from .helpers import next_book_id
 from .models import Book, BookRequest
 from .db import BOOKS
@@ -17,6 +17,7 @@ async def read_book(book_id: int = Path(gt=0)):
     for book in BOOKS:
         if book.id == book_id:
             return book
+    raise HTTPException(status_code=404, detail='Item not found')
 
 # books by publish_date
 @app.get('/books/publish/')
@@ -51,14 +52,21 @@ async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
 # update a book
 @app.put('/book/update_book')
 async def update_book(book: BookRequest):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Update unsuccessful, book not found.')
 
 # delete a book
 @app.delete('/books/{book_id}')
 async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book_id:
             deleted_book = BOOKS.pop(i)
             return deleted_book
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Update unsuccessful, book not found.')
