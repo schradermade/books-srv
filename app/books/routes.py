42c02f69e2c44e730b024659daf39f29244a692f
Nathan Schrader <natecschrader@gmail.com>
2024-07-21 11:32:29 -0700

    add books by rating endpoint

--- app/books/routes.py
+++ app/books/routes.py
@@ -5,16 +5,19 @@ from .db import BOOKS
 
 app = FastAPI()
 
+# All books
 @app.get('/books')
 async def read_all_books():
     return BOOKS
 
+# Book by id
 @app.get('/books/{book_id}')
 async def read_book(book_id: int):
     for book in BOOKS:
         if book.id == book_id:
             return book
 
+# new book
 @app.post('/create_book')
 async def create_book(book_request: BookRequest):
     book_data = book_request.model_dump()
@@ -22,4 +25,15 @@ async def create_book(book_request: BookRequest):
     new_book = Book(**book_data)
     BOOKS.append(new_book)
     return new_book
+
+# books by rating
+@app.get('/books/')
+async def read_book_by_rating(book_rating: int):
+    books_to_return = []
+
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
 
